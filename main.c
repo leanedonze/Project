641274336df5b0c5c3c18eeba43e011ff65c28ee
leanedonze <48278352+leanedonze@users.noreply.github.com>
2021-04-09 14:51:35 +0200

    Début

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <leds.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <main.h>
+
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    set_led(LED7, 8);
+    set_led(LED3, 8);
+
+    /* Infinite loop. */
+    while (1) {
+    	//waits 1 second
+
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
