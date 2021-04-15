2f4b2e292bfbb0bfa89de5fca1426f1b6a4ef582
Alice <alice.guntli@epfl.ch>
2021-04-15 12:37:38 +0200

    proximity update - problèmes avec LEDs?

--- process_proximity.c
+++ process_proximity.c
@@ -15,6 +15,22 @@
 #define IR_FRONT_LEFT45		6
 #define IR_FRONT_LEFT		7
 
+#define NUMBER_SENSORS		8
+
+
+static bool ir_states[NUMBER_SENSORS];
+
+void ir_state(int sensor, int led ){
+	if (get_calibrated_prox(sensor)>IR_THRESHOLD){
+		ir_states[sensor] = true;
+		set_led(led,2);									//Temporary (for test)
+	}
+	else{
+		ir_states[sensor] = false;
+		set_led(led,0);
+	}
+}
+
 
 static THD_WORKING_AREA(waProximity, 256);
 static THD_FUNCTION(Proximity, arg) {
@@ -25,18 +41,68 @@ static THD_FUNCTION(Proximity, arg) {
 
     while(1){
 
+    	ir_state(IR_FRONT_RIGHT,LED1);
+    	ir_state(IR_FRONT_RIGHT45,LED2);
+    	ir_state(IR_RIGHT,LED3);
+    	ir_state(IR_BACK_RIGHT,LED5);
+    	ir_state(IR_BACK_LEFT,LED6);
+    	ir_state(IR_LEFT,LED7);
+    	ir_state(IR_FRONT_LEFT45,LED8);
+    	ir_state(IR_FRONT_LEFT,LED1);
+
+    	/*if (get_calibrated_prox(IR_FRONT_RIGHT)>IR_THRESHOLD){
+    		ir_states[IR_FRONT_RIGHT] = true;
+    		set_led(LED1,2);									//Temporary (for test)
+    	}
+    	else{
+    		ir_states[IR_FRONT_RIGHT] = false;
+    		set_led(LED1,0);
+    	}
+
+    	if (get_calibrated_prox(IR_FRONT_RIGHT45)>IR_THRESHOLD){
+    		ir_states[IR_FRONT_RIGHT45] = true;
+    		set_led(LED2,2);									//Temporary (for test)
+    	}
+    	else{
+    		ir_states[IR_FRONT_RIGHT45] = false;
+    		set_led(LED2,0);
+    	}
+
     	if (get_calibrated_prox(IR_RIGHT)>IR_THRESHOLD){
+    		ir_states[IR_RIGHT] = true;
     		set_led(LED3,2);									//Temporary (for test)
     	}
     	else{
+    		ir_states[IR_RIGHT] = false;
     		set_led(LED3,0);
     	}
+
+    	if (get_calibrated_prox(IR_BACK_RIGHT)>IR_THRESHOLD){
+    		ir_states[IR_BACK_RIGHT] = true;
+    		set_led(LED5,2);									//Temporary (for test)
+    	}
+    	else{
+    		ir_states[IR_BACK_RIGHT] = false;
+    		set_led(LED5,0);
+    	}
+
+    	if (get_calibrated_prox(IR_BACK_LEFT)>IR_THRESHOLD){
+    		ir_states[IR_BACK_LEFT] = true;
+    		set_led(LED6,2);									//Temporary (for test)
+    	}
+    	else{
+    		ir_states[IR_BACK_LEFT] = false;
+    		set_led(LED6,0);
+    	}
+
     	if (get_calibrated_prox(IR_LEFT)>IR_THRESHOLD){
+    		ir_states[IR_LEFT] = true;
     		set_led(LED7,2);
     	}
     	else{
+    		ir_states[IR_LEFT] = false;
     		set_led(LED7,0);
-    	}
+    	}*/
 
 
         chThdSleepMilliseconds(SLEEP50HZ); // To be determined
